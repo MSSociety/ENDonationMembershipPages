7e9f2ae1ccf0207058b5cd0c6e938fe98671b8ce
Joe Hall <joefhall@gmail.com>
2019-05-02 14:25:54 +0000

    Add hamburger menu on mobile

--- page-builder-scripts-v1.1.js
+++ page-builder-scripts-v1.1.js
@@ -7,4 +7,28 @@ function moveFloatingContent() {
 
 $(document).ready(function() {
   moveFloatingContent();
+  
+  $('.hamburger').click(function () {
+    var hamburger = $('.hamburger');
+    
+    if (hamburger.hasClass('is-active')) {
+      hamburger.removeClass('is-active');
+      $('#top-menu-links').addClass('hide-mobile');
+      $('#top-menu-links').css('display', '');
+    } else {
+      hamburger.addClass('is-active');
+      $('#top-menu-links').removeClass('hide-mobile');
+      $('#top-menu-links').css('display', 'flex');
+    }
+  });
+  
+  $(document).click(function(event) { 
+    var target = $(event.target);
+    if(!target.closest('.hamburger').length
+       && $('.hamburger').hasClass('is-active')) {
+      $('.hamburger').removeClass('is-active');
+      $('#top-menu-links').addClass('hide-mobile');
+      $('#top-menu-links').css('display', '');
+    }        
+  });
 });
