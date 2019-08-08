fd9b481605c1ac124a18ce82a5f145eba8853e16
joefhall <joefhall@gmail.com>
2019-08-08 06:40:12 -0400

    Set up basic display of question, answers, explanation, next

--- page-builder-scripts-quiz-v1.1.js
+++ page-builder-scripts-quiz-v1.1.js
@@ -2,7 +2,15 @@ function initialiseQuiz() {
   $('.quiz-question').each(function(questionIndex) {
     var questionNumber = questionIndex + 1;
     var inputName = 'question' + questionNumber;
-    
+
+    $(this).prepend('<h1 class="quiz-question-number">' + questionNumber + ' of ' + window.quiz.questionsTotal + '</h1>');
+
+    $(this).append('<div class="quiz-question-next">Next <img src="https://MSSociety.github.io/ENDonationMembershipPages/images/arrow-down.svg" alt=""></div>');
+    var nextQuestionNumber = (questionNumber < window.quiz.questionsTotal) ? questionNumber + 1 : 'signup';
+    $(this).find('.quiz-question-next').click(function() {
+      nextQuizQuestion(nextQuestionNumber);
+    });
+
     $(this).find('.quiz-answer').each(function(answerIndex) {
       var input = $(this).children('.quiz-answer-input');
       var id = inputName + answerIndex;
@@ -13,9 +21,13 @@ function initialiseQuiz() {
   });
 }
 
+function nextQuizQuestion(questionNumber) {
+  console.log(questionNumber);
+}
+
 $(document).ready(function() {
   window.quiz = {};
-  window.quiz.questions = $('.quiz-question').length;
+  window.quiz.questionsTotal = $('.quiz-question').length;
   
   initialiseQuiz();
   
