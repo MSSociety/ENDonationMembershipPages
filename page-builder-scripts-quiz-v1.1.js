7c9e5a2b3d59223cbc77e31c1d89de332f1472ee
joefhall <joefhall@gmail.com>
2019-08-08 10:15:10 -0400

    Quiz - display next question, validate form, display finish button

--- page-builder-scripts-quiz-v1.1.js
+++ page-builder-scripts-quiz-v1.1.js
@@ -1,14 +1,64 @@
+(function ($) {
+  $.fn.isAfter = function ($elm){
+    var $this = $(this);
+    var $myParents = $this.parents();
+    var $elmParents = $elm.parents();
+
+    var $myTreeLast = $this;
+
+    var level = 0;
+    for(var i in $myParents)
+    {
+      var $elmTreeLast = $elm;
+      if (!$myParents.hasOwnProperty(i))
+      {
+        continue;
+      }
+      var $parent = $($myParents[i]);
+
+      for (var j in $elmParents)
+      {
+        if (!$elmParents.hasOwnProperty(j))
+        {
+          continue;
+        }
+
+        var $elmParent = $($elmParents[j]);
+
+        if ($parent[0] === $elmParent[0])
+        {
+          var myTreePos = $myTreeLast.index();
+          var elmTreePos = $elmTreeLast.index();
+          return (myTreePos > elmTreePos);
+        }
+
+        $elmTreeLast = $elmParent;
+      }
+      $myTreeLast = $parent;
+    }
+
+    return false;
+  }
+})(jQuery);
+
 function initialiseQuiz() {
+  var questionsTotal = $('.quiz-question').length;
+  var signupPosition = ($('.quiz-form').isAfter($('.quiz-question'))) ? 'after' : 'before';
+
   $('.quiz-question').each(function(questionIndex) {
     var questionNumber = questionIndex + 1;
-    var nextQuestionNumber = (questionNumber < window.quiz.questionsTotal) ? questionNumber + 1 : 'signup';
+    var nextQuestionNumber = (questionNumber < questionsTotal) ? questionNumber + 1 : (signupPosition === 'after' ? 'signup' : 'finish');
     var inputName = 'question' + questionNumber;
 
     $(this).prop('id', 'quiz-question' + questionNumber);
 
-    $(this).prepend('<h1 class="quiz-question-number">' + questionNumber + ' of ' + window.quiz.questionsTotal + '</h1>');
+    $(this).prepend('<h1 class="quiz-question-number">' + questionNumber + ' of ' + questionsTotal + '</h1>');
 
-    $(this).append('<div class="quiz-question-next-holder"><div class="quiz-question-next" onClick="nextQuizQuestion(' + questionNumber + ', ' + nextQuestionNumber + ')">Next <img src="https://MSSociety.github.io/ENDonationMembershipPages/images/arrow-down.svg" alt=""></div></div>');
+    if (questionNumber === questionsTotal && signupPosition === 'before') {
+      $(this).append('<div class="quiz-question-finish" onClick="$(\'form\').submit()">Finish</div>');
+    } else {
+      $(this).append('<div class="quiz-question-next-holder"><div class="quiz-question-next" onClick="nextQuizQuestion(' + questionNumber + ', ' + nextQuestionNumber + ')">Next <img src="https://MSSociety.github.io/ENDonationMembershipPages/images/arrow-down.svg" alt=""></div></div>');
+    }
 
     $(this).find('.quiz-answer').each(function(answerIndex) {
       var input = $(this).children('.quiz-answer-input');
@@ -26,15 +76,31 @@ function initialiseQuiz() {
       });
     });
 
-    if (questionNumber === 1) {
+    if (questionNumber === 1 && signupPosition === 'after') {
       $(this).css('display', 'flex');
+    }
+  });
+
+  $('.quiz-question-start').click(function() {
+    window.validateAllFields();
+    if ($('.en__field__error').length) {
+      $('html, body').animate({
+        scrollTop: $('.en__field__error').first().offset().top
+      }, 500);
+    } else {
+      nextQuizQuestion('signup', 1);
     }
   });
 }
 
 function nextQuizQuestion(questionNumber, nextQuestionNumber) {
   var question = $('#quiz-question' + questionNumber);
-  var nextQuestion = $('#quiz-question' + nextQuestionNumber);
+  var nextQuestion;
+  if (nextQuestionNumber === 'signup') {
+    nextQuestion = $('.quiz-form');
+  } else {
+    nextQuestion = $('#quiz-question' + nextQuestionNumber);
+  }
 
   nextQuestion.css('display', 'flex');
   $([document.documentElement, document.body]).animate({
@@ -59,10 +125,5 @@ function revealAnswer(questionNumber) {
 }
 
 $(document).ready(function() {
-  window.quiz = {};
-  window.quiz.questionsTotal = $('.quiz-question').length;
-  
   initialiseQuiz();
-  
-  console.log(window.quiz);
 });
